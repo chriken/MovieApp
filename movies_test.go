630f4affd7fddb9b16ed67021aeac11fb64a857e
Christian Karlsson <christian.karlsson@bonniernews.se>
2021-04-08 15:23:08 +0200

    Added simple test case

--- movies_test.go
+++ movies_test.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSomething(t *testing.T) {
+	assert := assert.New(t)
 
 	//Checks that first element in list is Fargo
 	allmovies := Movies
 	movie := allmovies[0]
-	assert.Contains(t, movie.Title, "Fargo")
+	assert.Contains("Fargo", movie.Title)
 
+	//Checks that there is 5 titles in the movie list
+	var nr int
+	for k := range allmovies {
+		nr = k + 1
+	}
+	assert.Equal(5, nr)
 }
